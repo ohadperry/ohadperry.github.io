1f0036f00fe38fb86148fe827bd3a66319640fae
Ohad Perry <ohadpartuck@gmail.com>
2015-12-12 12:32:10 +0200

    removed default values

--- solve-my-problem/assets/in_house/js/backend_integration.js
+++ solve-my-problem/assets/in_house/js/backend_integration.js
@@ -9,15 +9,14 @@ $(function() {
         init_app: function(){
             Parse.initialize("2dFgWLAPHrXIBU3SQ7I1j3q0ErhR0UcbIMNXg3LP",
                 "qe0M53EBz8dZJGfpmWG4N67zWsmtayKhbO6CPKOg");
-
-            var query = new Parse.Query('Problem');
-            query.find({
-                success: function(problems) {
-                    for (var i = 0; i < problems.length; ++i) {
-                        console.log(problems[i].attributes);
-                    }
-                }
-            });
+            //var query = new Parse.Query('Problem');
+            //query.find({
+            //    success: function(problems) {
+            //        for (var i = 0; i < problems.length; ++i) {
+            //            console.log(problems[i].attributes);
+            //        }
+            //    }
+            //});
 
         }
     };
